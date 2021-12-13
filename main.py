02b9d6eacc2bea2252cf9c56a8615e3cd027ea24
diadntjr <dntjr0501@gmail.com>
2021-12-13 11:01:05 +0900

    Modified main.py
    It can print Graph

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import csv
 import matplotlib.pyplot as plt
 
-def printcheckcsvfile():
-    print(data)
-
 def printlocation():
     inputData = str(input("원하시는 지역을 입력해주세요! "))
     for row in data:
@@ -11,33 +8,33 @@ def printlocation():
             print(row)
     return inputData
 
-def printSearchData(inputData):
+def printSearchData(InputData):
+    result = []
+    name = []
     for row in data:
-        if '경상남도' in row[2]:
-            for i in row[6:7]:
+        if '부산광역시' in row[2]:
+            for j in row[5::6]:
+                result.append(int(j))
+            for i in row[0:1]:
                 name.append(i)
-            for j in row[5:6]:
-                result.append(j)
     print(name)
     print(result)
     return result, name
 
-def printGraph(inputData, name, result):
-    plt.figure(dpi=150)
-    plt.figure(figsize=(20, 8))
+def printGraph(InputData, name, result):
+    plt.figure(dpi=1200)
+    plt.figure(figsize=(40, 30))
     plt.rc('font', family='NanumGothic')
-    plt.title(inputData + '의 전통시장 점포수 현황')
+    plt.title('부산광역시의 전통시장 점포수 현황')
     plt.xlabel('점포수')
-    plt.ylabel('지역')
+    plt.ylabel('시장이름')
     plt.barh(name, result, color='tomato')
     plt.show()
+    plt.savefig("test.jpg", format='jpg', dpi=1200)
+
 
 if __name__ == '__main__' :
-    result = []
-    name = []
     f = open('./csv_file/소상공인시장진흥공단_전통시장현황_02_19_2021-2.csv', 'r')
     data = csv.reader(f)
-    printcheckcsvfile()
-    inputData = printlocation()
-    printSearchData(inputData)
-   # printGraph(inputData, name, result)
+    result, name = printSearchData()
+    printGraph(name, result)
